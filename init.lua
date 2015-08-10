2e53bb331817ebb695f6c5d1c6867c6362038386
joz <joz@spline.de>
2015-08-10 21:20:56 +0200

    Made grass persistent

--- init.lua
+++ init.lua
@@ -15,12 +15,15 @@ minetest.register_on_generated(function (minp,maxp, seed)
 
 	-- read content ids
 	local c_air     = minetest.get_content_id("air")
+	local c_ignore  = minetest.get_content_id("ignore")
 	local c_stone   = minetest.get_content_id("stone")
 	local c_dirt    = minetest.get_content_id("dirt")
 	local c_dirt_wg = minetest.get_content_id("dirt_with_grass")
 	local c_grass   = {}
+	local blacklist_air = {[c_stone]=true, [c_dirt]=true, [c_dirt_wg]=true}
 	for i = 1, 5 do
 		c_grass[i] = minetest.get_content_id("default:grass_"..i)
+		blacklist_air[c_grass[i]]=true
 	end
 
 	-- read chunk data
@@ -40,27 +43,36 @@ minetest.register_on_generated(function (minp,maxp, seed)
 	for z = emin.z,emax.z do
 		for y = emin.y,emax.y do
 			for x = emin.x,emax.x do
-				-- which material?
-				if density_map[nixyz] < 0 then
-					data[nixyz] = c_air
-				elseif density_map[nixyz] > 0.15 then
-					data[nixyz] = c_stone
-				elseif y < emax.y and density_map[nixyz+chulens.x] < 0 then -- data[x,y+1,z] == air?
-					-- top border between lump and air
-					data[nixyz] = c_dirt_wg
-					-- generate plants?
-					local random_number = pr:next() -- 0..32767
-					if random_number < 8192 then
-						-- grass
-						local grass_number = random_number % 5 + 1
-						data[nixyz + chulens.x] = c_grass[grass_number] -- data[x,y+1,z] = grass
-					end
-				else
-					data[nixyz] = c_dirt
-				end-- if density
+				if true then
+					-- which material?
+					if density_map[nixyz] < 0 then
+						if not blacklist_air[data[nixyz]] then
+							data[nixyz] = c_air
+						end
+					elseif density_map[nixyz] > 0.15 then
+						data[nixyz] = c_stone
+					elseif y < emax.y and density_map[nixyz+chulens.x] < 0 then -- data[x,y+1,z] == air?
+						-- top border between lump and air
+						data[nixyz] = c_dirt_wg
+						-- generate plants?
+						local random_number = pr:next() -- 0..32767
+						if random_number < 8192 then
+							-- grass
+							local grass_number = random_number % 5 + 1
+							data[nixyz + chulens.x] = c_grass[grass_number] -- data[x,y+1,z] = grass
+						end
+					else
+						data[nixyz] = c_dirt
+					end-- if density
 
-				-- next index
-				nixyz = nixyz + 1
+					-- next index
+					nixyz = nixyz + 1
+				else
+					--print("node.name: "..vm:get_node_at({x=x,y=y,z=z}).name)
+					--print("node.id: "..data[nixyz])
+					--print('minetest.get_content_id("air"): '.. c_air)
+					--print("what the")
+				end-- if data
 			end-- for x
 		end-- for y
 	end-- for z
