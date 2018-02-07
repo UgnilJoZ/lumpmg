2fc4f6e44e6e126e05eca617f70769fb8fbbcbbf
Gael-de-Sailly <gael.chretien@akeonet.com>
2018-02-07 21:34:17 +0100

    Change algorithm, to have y in the last loop. This allows dealing with above and below nodes more easily.

--- init.lua
+++ init.lua
@@ -34,60 +34,60 @@ minetest.register_on_generated(function(minp, maxp, seed)
 	local data = vm:get_data()
 
 	local side_length = maxp.x - minp.x + 1
-	local biglen = side_length+32
+	local biglen = emax.x - emin.x + 1
 
-	local chulens = {x=side_length, y=side_length, z=side_length}
+	local chulens = {x=side_length, y=side_length+2, z=side_length}
+	local a = VoxelArea:new({MinEdge = emin, MaxEdge = emax})
+	local ystride = a.ystride
 
 	-- generate noise data
-	local density_map = minetest.get_perlin_map(np_density, chulens):get3dMap_flat(minp)
+	local density_map = minetest.get_perlin_map(np_density, chulens):get3dMap_flat({x=minp.x, y=minp.y-1, z=minp.z})
 
 	-- initialize perlin map and data index
 	local nixyz = 1
 	local dixyz = 1
 
+	local decrX = side_length^2*(side_length+2)-1 -- Decrement index every X line
+	--print(decrX)
+
 	-- iterate through data and fill with materials
-	dixyz = dixyz+16*biglen*biglen
-	for z = minp.z,maxp.z do
-		dixyz = dixyz+16*biglen
-		for y = minp.y,maxp.y do
-			dixyz = dixyz+16
-			for x = minp.x,maxp.x do
-				local density = density_map[nixyz]
-				-- which material?
-				if density < 0 then
-					if not blacklist_air[data[dixyz]] then
-						data[dixyz] = c_air
-					end
-				elseif density > 0.15 then
-					-- stone
+	for x = minp.x, maxp.x do
+		for z = minp.z, maxp.z do
+			--print(x-minp.x, z-minp.z, nixyz)
+			dixyz = a:index(x, minp.y-1, z)
+			local density_below = density_map[nixyz] -- Node below
+			nixyz = nixyz + side_length
+			local density = density_map[nixyz]
+			for y = minp.y, maxp.y do
+				nixyz = nixyz + side_length
+				local density_above = density_map[nixyz] -- Node above
+				if density > 0.15 then
 					data[dixyz] = c_stone
-				elseif y < maxp.y -- density map is just calculated from minp to maxp
-				and density_map[nixyz+side_length] < 0 then -- data[x,y+1,z] == air?
-					-- top border between lump and air
-					data[dixyz] = c_dirt_wg
+				elseif density > 0 then
+					if density_above < 0 then
+						data[dixyz] = c_dirt_wg
+					elseif density_below < 0 then
+						data[dixyz] = c_obsidian
+					else
+						data[dixyz] = c_dirt
+					end
+				elseif density_below > 0 then
+					-- top border between lump and air (air side)
 					-- generate plants?
 					local random_number = pr:next() -- 0..32767
 					if random_number < 8192 then
 						-- grass
 						local grass_number = random_number % 5 + 1
-						data[dixyz + biglen] = c_grass[grass_number] -- data[x,y+1,z] = grass
+						data[dixyz] = c_grass[grass_number] -- data[x,y+1,z] = grass
 					end
-				elseif y > minp.y
-				and density_map[nixyz-side_length] < 0 then
-					data[dixyz] = c_obsidian
-				else
-					data[dixyz] = c_dirt
-				end-- if density
-
-				-- next index
-				nixyz = nixyz + 1
-				dixyz = dixyz + 1
-			end-- for x
-			dixyz = dixyz+16
-		end-- for y
-		dixyz = dixyz+16*biglen
-	end-- for z
-	--dixyz = dixyz+16*biglen*biglen
+				end
+				density_below, density = density, density_above -- Offset density variables for next node
+				dixyz = dixyz + ystride
+			end
+			nixyz = nixyz + side_length
+		end
+		nixyz = nixyz - decrX
+	end
 
 	-- write back the chunk
 	vm:set_data(data)
